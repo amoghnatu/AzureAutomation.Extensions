f1eea30da9b859660dbc87d38c45e9ce68cd99a3
Amogh Natu <natu.amogh@gmail.com>
2018-06-03 15:07:23 +0530

    Fixed issues in Runbook import and also raised an open issue in github https://github.com/Azure/azure-powershell/issues/6368

--- AzureAutomation.Extensions/cmdlets/AzureAutomation.Extensions.psm1
+++ AzureAutomation.Extensions/cmdlets/AzureAutomation.Extensions.psm1
@@ -26,6 +26,8 @@ function Get-RunbookExtension {
 		"WorkFlow" { $extension = ".ps1" }
 		"GraphPowerShellWorkflow" { $extension = ".graphrunbook" }
 	}
+
+    $extension
 }
 function Clone-AutomationAccount {
 	param(
@@ -63,7 +65,7 @@ function Clone-AutomationAccount {
 	Switch-AzureContext -SubscriptionId $SourceSubscriptionId
 	$SourceSubscriptionContext = Get-AzureRmContext
 	$DestinationSubscriptionContext = $null
-	if ($SourceSubscriptionId -ne $DestinationSubscriptionId) {
+	if (!([string]::IsNullOrWhiteSpace($DestinationSubscriptionId)) -and ($SourceSubscriptionId -ne $DestinationSubscriptionId)) {
 		Switch-AzureContext -SubscriptionId $DestinationSubscriptionId
 		$DestinationSubscriptionContext = Get-AzureRmContext
 
@@ -83,14 +85,15 @@ function Clone-AutomationAccount {
 
 	$destAA = Get-AzureRmAutomationAccount -ResourceGroupName $DestinationAutomationAccountResourceGroup -Name $DestinationAutomationAccount -AzureRmContext $DestinationSubscriptionContext -ErrorAction SilentlyContinue
 	if ($destAA) {
-		throw "Provided Destination Automation Account is already available in Resource Group $DestinationAutomationAccountResourceGroup in subscription $DestinationSubscriptionId"
+		Write-Warning "Provided Destination Automation Account is already available in Resource Group $DestinationAutomationAccountResourceGroup in subscription $DestinationSubscriptionId"
 	}
+    else {
+	    $NewAA = New-AzureRmAutomationAccount -ResourceGroupName $DestinationAutomationAccountResourceGroup -Name $DestinationAutomationAccount -Location $SrcAA.Location -Plan $SrcAA.Plan -Tags $SrcAA.Tags -Verbose -ErrorAction Stop -AzureRmContext $DestinationSubscriptionContext
 
-	$NewAA = New-AzureRmAutomationAccount -ResourceGroupName $DestinationAutomationAccountResourceGroup -Name $DestinationAutomationAccount -Location $SrcAA.Location -Plan $SrcAA.Plan -Tags $SrcAA.Tags -Verbose -ErrorAction Stop -AzureRmContext $DestinationSubscriptionContext | Write-Verbose
-
-	if (-not $NewAA) {
-		throw "Failed to create automation account"
-	}
+	    if (-not $NewAA) {
+	    	throw "Failed to create automation account"
+	    }
+    }
 
 	#endregion
 
@@ -101,24 +104,52 @@ function Clone-AutomationAccount {
 	else {
 		Write-Output "Beginning copying of runbooks from $SourceAutomationAccount to $DestinationAutomationAccount"
 		$WorkFolder = New-TempFolder
-		$RunbooksInSourceAutomationAccount = Get-AzureRmAutomationRunbook -ResourceGroupName $SourceAutomationAccountResourceGroup -AutomationAccountName $SourceAutomationAccount
+        $RunbooksInSourceAutomationAccount = Get-AzureRmAutomationRunbook -ResourceGroupName $SourceAutomationAccountResourceGroup -AutomationAccountName $SourceAutomationAccount
 		if ($null -eq $RunbooksInSourceAutomationAccount) {
 			Write-Warning "There are no runbooks in automation account $SourceAutomationAccount. Proceeding to next step."
 		}
 		else {
 			$RunbooksInSourceAutomationAccount | ForEach-Object {
 				Write-Output "Exporting runbook : $($_.Name) from Automation Account : $SourceAutomationAccount"
-				$null = Export-AzureRmAutomationRunbook -Name $_.Name -ResourceGroupName $SourceAutomationAccountResourceGroup -AutomationAccountName $SourceAutomationAccount -OutputFolder $WorkFolder -Slot Published -Force -AzureRmContext $SourceSubscriptionContext
+				$null = Export-AzureRmAutomationRunbook -Name $_.Name -ResourceGroupName $SourceAutomationAccountResourceGroup -AutomationAccountName $SourceAutomationAccount -OutputFolder $WorkFolder -Force -AzureRmContext $SourceSubscriptionContext
+				$extension = Get-RunbookExtension -RunbookType $_.RunbookType
+                
+                # IF type is Python, set extension temporarily to .ps1 due to an open bug in Export cmdlet
+                # https://github.com/Azure/azure-powershell/issues/6368
+                if ($_.RunbookType -eq "Python2")
+                {
+                    $extension = ".ps1"
+                }
 				if (!(Test-Path -Path (Join-Path $WorkFolder ($($_.Name) + $extension)))) {
 					Write-Warning "Failed to export runbook : $($_.Name) from Source Automation Account : $SourceAutomationAccount. Skipping importing this."
 				}
 				else {
-					$extension = Get-RunbookExtension -RunbookType $_.RunbookType
+                    # rename the extension to python if original runbook's type is python
+                    if ($_.RunbookType -eq "Python2")
+                    {
+                        Rename-Item -Path (Join-Path $WorkFolder ($($_.Name) + $extension)) -NewName (Join-Path $WorkFolder ($($_.Name) + ".py")) -PassThru -Force
+
+                        # Reset extension back to python
+                        $extension = Get-RunbookExtension -RunbookType $_.RunbookType
+                    }
 					Write-Output "Importing runbook : $($_.Name) into Automation Account : $DestinationAutomationAccount"
-					$null = Import-AzureRmAutomationRunbook -Path (Join-Path $WorkFolder ($($_.Name) + $extension)) -Description $_.Description -Name $_.Name -Tags $_.Tags -Type $_.RunbookType -Published -ResourceGroupName $DestinationAutomationAccountResourceGroup -AutomationAccountName $DestinationAutomationAccount -Force
+                    $RunbookType = ""
+                    if ($_.RunbookType -eq "GraphPowerShell")
+                    {
+                        $RunbookType="GraphicalPowerShell"
+                    }
+                    elseif ($_.RunbookType -eq "GraphPowerShellWorkflow")
+                    {
+                        $RunbookType = "GraphicalPowerShellWorkflow"
+                    }
+                    else
+                    {
+                        $RunbookType = $_.RunbookType
+                    }
+					$null = Import-AzureRmAutomationRunbook -Path (Join-Path $WorkFolder ($($_.Name) + $extension)) -Description $_.Description -Name $_.Name -Tags $_.Tags -Type $RunbookType -Published -ResourceGroupName $DestinationAutomationAccountResourceGroup -AutomationAccountName $DestinationAutomationAccount -Force
 				}
 				Write-Output "========================================================"
-			}
+            }
 		}
 		Write-Output "Copying of runbooks completed."
 	}
